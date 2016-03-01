24216a6748b1e70427431238d2be1588746075bb
Spencer Williams <spencer@aninternetpresence.net>
2016-02-29 21:02:02 -0700

    Use MV* pattern
    - Not working in Firefox at the moment

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 udacity-frontend-attendance
 ===========================
+
+My solution to the JavaScript Design Patterns project where a less refined solution for an attendence app had to be refactored with an MV* approach.
+
+## Bugs
+* Not working on Firefox
--- js/app.js
+++ js/app.js
@@ -29,56 +29,88 @@
 
 /* STUDENT APPLICATION */
 $(function() {
-    var attendance = JSON.parse(localStorage.attendance),
-        $allMissed = $('tbody .missed-col'),
-        $allCheckboxes = $('tbody input');
+   var model = {
+       init: function(){
+           this.attendance = JSON.parse(localStorage.attendance);
+       },
+       getAttendance: function(){
+           return this.attendance;
+       },
+       setAttendance: function(data){
+           this.attendance = data;
+           localStorage.attendance = JSON.stringify(data);
+       }
+   };
+   var octopus = {
+       init: function(){
+           model.init();
+           view.init();
+       },
+       getAttendance: function(){
+           return model.getAttendance();
+       },
+       setAttendance: function(newAttendance){
+           var data = this.getAttendance();
+           
+           data[newAttendance.name][newAttendance.day] = newAttendance.attended;
+           model.setAttendance(data);
+       }
+   };
+   var view = {
+       init: function(){
+           this.studentRows = $('.student');
+           this.studentHeader = $('thead');
+           this.render();
+       },
+       render: function(){
+           var attendance = octopus.getAttendance();
+           this.studentRows.each(function(studentRowIndex, studentRowElement){
+              function updateDaysMissed(){
+                 missed_col.text($.grep(octopus.getAttendance()[studentName], function(element, index){
+                       return element;
+                  }).length);
+               }
+               var studentName = $(studentRowElement).find('.name-col').text();
+               var studentAttendance = attendance[studentName];
+               var missed_col = $(studentRowElement).find('.missed-col');
+               var missedDays = 0;
 
-    // Count a student's missed days
-    function countMissing() {
-        $allMissed.each(function() {
-            var studentRow = $(this).parent('tr'),
-                dayChecks = $(studentRow).children('td').children('input'),
-                numMissed = 0;
+               $.each(studentAttendance, function(day, attended){
+                   var headerFilled = view.studentHeader.find('.name-col').hasClass('filled');
 
-            dayChecks.each(function() {
-                if (!$(this).prop('checked')) {
-                    numMissed++;
-                }
-            });
+                   if(!headerFilled){
+                      var th = $(document.createElement('th'));
+                      th.text(day + 1);
+                      view.studentHeader.find('.missed-col').before(th);
 
-            $(this).text(numMissed);
-        });
-    }
+                      if(day === 11){
+                         view.studentHeader.find('.name-col').addClass('filled');
+                      }
+                   }
 
-    // Check boxes, based on attendace records
-    $.each(attendance, function(name, days) {
-        var studentRow = $('tbody .name-col:contains("' + name + '")').parent('tr'),
-            dayChecks = $(studentRow).children('.attend-col').children('input');
+                   var attend_col = $(document.createElement('td'));
+                   var checkbox = $(document.createElement('input'));
+                   checkbox.attr('type', 'checkbox').prop('checked', attended);
+                   
+                   checkbox.on('change', function(){
+                         var newAttendance = {
+                           name: studentName,
+                           day: day,
+                           attended: checkbox.prop('checked')
+                         };
 
-        dayChecks.each(function(i) {
-            $(this).prop('checked', days[i]);
-        });
-    });
+                         octopus.setAttendance(newAttendance);
 
-    // When a checkbox is clicked, update localStorage
-    $allCheckboxes.on('click', function() {
-        var studentRows = $('tbody .student'),
-            newAttendance = {};
-
-        studentRows.each(function() {
-            var name = $(this).children('.name-col').text(),
-                $allCheckboxes = $(this).children('td').children('input');
-
-            newAttendance[name] = [];
-
-            $allCheckboxes.each(function() {
-                newAttendance[name].push($(this).prop('checked'));
-            });
-        });
-
-        countMissing();
-        localStorage.attendance = JSON.stringify(newAttendance);
-    });
-
-    countMissing();
+                         updateDaysMissed();
+                   });
+                   
+                   attend_col.attr('class', 'attend-col').append(checkbox);                            
+                   missed_col.before(attend_col);
+               });
+               updateDaysMissed();               
+           });
+       }
+   };
+   
+   octopus.init();
 }());
